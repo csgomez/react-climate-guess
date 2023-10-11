6b3fef3e7c98b1f83539d49c5b452e0b1e03dcf2
Cesar <csgomez@users.noreply.github.com>
2023-10-11 17:37:26 -0500

    Moved the mockCityProps outside of a specific test so it can be reused in other tests.

--- src/components/CityCard.test.tsx
+++ src/components/CityCard.test.tsx
@@ -1,11 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import { City } from '../types';
 import CityCard from './CityCard';
 
 describe('CityCard', () => {
-  it('renders its props correctly', () => {
-    /* Create mock props for CityCard component */
-    const mockCity: City = {
+  const mockCityProps = {
+    city: {
       flag: '🇵🇪',
       coords: {
         lat: -12.05,
@@ -13,30 +11,22 @@ describe('CityCard', () => {
       },
       location: 'Peru',
       name: 'Lima',
-    };
+    },
+    temperature: 65.7,
+    onSelectCity: () => {
+      return;
+    },
+    selectedCity: null,
+    warmerCity: null,
+  };
 
-    const mockTemp = 60.5;
-
-    const mockOnSelectCity = (city: City) => {
-      console.log('Current city:', city.name);
-    };
-
-    const mockSelectedCity = null;
-
-    const mockWarmerCity = null;
-
-    const mockProps = {
-      city: mockCity,
-      temperature: mockTemp,
-      onSelectCity: mockOnSelectCity,
-      selectedCity: mockSelectedCity,
-      warmerCity: mockWarmerCity,
-    };
-
-    /* Rendering and testing state */
-    render(<CityCard {...mockProps} />);
+  it('renders its props correctly', () => {
+    render(<CityCard {...mockCityProps} />);
 
     expect(screen.getByText(/Lima/)).toBeInTheDocument();
-    expect(screen.getByText);
+    expect(screen.getByText(/Peru/)).toBeInTheDocument();
+
+    const chooseButton = screen.getByRole('button');
+    expect(chooseButton).toBeInTheDocument();
   });
 });
